24b8a3303435633aad1a9ce3192c5f436ece934d
linkup <446720283@qq.com>
2022-03-18 23:44:55 +0800

    idea 提交

---
+++ src/main/java/com/example/mybatisPlus/GitTest.java
@@ -0,0 +1,7 @@
+package com.example.mybatisPlus;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git commit 1");
+    }
+}
--- src/main/java/com/example/mybatisPlus/pojo/User.java
+++ src/main/java/com/example/mybatisPlus/pojo/User.java
@@ -6,11 +6,12 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.example.mybatisPlus.enums.SexEnums;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data  //除有参构造外，其余的都给补充了，可以从编译后的target文件夹查看（maven记得clean 和重新compile）
 //@TableName() 设置表名
-
+@AllArgsConstructor
 public class User {
     @TableId(value = "id",type = IdType.AUTO )
     private Long id;
