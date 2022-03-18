d6e1c98ae243d89577b8a4b2f920d45cbcf5d276
linkup <446720283@qq.com>
2022-03-19 00:04:35 +0800

    git merge hot-fix 1

--- src/main/java/com/example/mybatisPlus/GitTest.java
+++ src/main/java/com/example/mybatisPlus/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git commit 1");
         System.out.println("git commit 2");
+        System.out.println("git merge hot-fix 1");
     }
 }
